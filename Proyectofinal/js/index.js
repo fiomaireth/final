0412be1e02fbf39eba41350f19967dc85b708acf
fiomaireth <soffstore.pe@gmail.com>
2022-10-31 09:50:31 -0500

    first commit

---
+++ Proyectofinal/js/index.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+    Swal.fire({
+        title: 'BIENVENIDO!!',
+        width: 600,
+        padding: '3em',
+        color: '#716add',
+        background: '#fff url("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beach-quotes-1559667853.jpg?crop=1.00xw:0.751xh;0,0.202xh&resize=1200:*")',
+        timer: 2500,
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("https://i.gifer.com/origin/e8/e85e517141772e97fec5fa4fceb81f39.gif")
+          left top
+          no-repeat
+        `
+
+      })
+    
+    }
